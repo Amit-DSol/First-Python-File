6cbeca478647cd104b1ff5e68e5cb40a1610c8bd
Amit-DSol <amitahirwarr2009@gmail.com>
2026-02-26 21:23:57 +0530

    More basics

--- First_program.py
+++ First_program.py
@@ -1,9 +1,19 @@
 # Starting Python
 print("Hello World")
 
-# Basics
+# Basics(variables)
 a = 5
 b = 8
 c = a+b 
 d = a-b
 print(c,d)
+
+# Basics(data types)
+str = "Amit"
+num = 56
+float = 45.4
+tuple = ()
+print(type(str))
+print(type(num))
+print(type(tuple))
+print(type(float))
